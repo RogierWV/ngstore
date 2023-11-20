dc5fa2c7e0e192d71e8dd6e777ec355069151c8e
Rogier Velting <rogiervelting@gmail.com>
2023-11-20 11:27:42 +0100

    try relative path for json

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -35,7 +35,7 @@ export class CartService {
   getShippingPrices() {
     return this.http.get
     <{type: string, price: number}[]>(
-      '/assets/shipping.json'
+      'assets/shipping.json'
     );
   }
 }
